a32124dcc5700b986ea4bd7d9cd93abeedda9ff7
vaishali232 <51689354+vaishali232@users.noreply.github.com>
2019-06-11 11:56:42 +0530

    Create vowelcons.c

---
+++ vowelcons.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+char s;
+scanf("%c",&s);
+if((s>='a'&&s<='z')||(s>='A'&&s<='Z'))
+{
+if(s=='a'&&s=='e'&&s=='i'&&s=='o'&&s=='u'&&s=='A'&&s=='E'&&s=='I'&&s=='O'&&s=='U')
+{
+printf("Vowel");
+}
+else
+{
+printf("Consonant");
+}
+}
+else
+{
+
+printf("invalid");
+
+}
+}
